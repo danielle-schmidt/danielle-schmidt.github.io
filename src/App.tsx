3d19001590cf5abda544cb4d87e4a4bf78798ada
Nikki Schmidt <nikki@nsylv.dev>
2025-08-09 13:42:10 -0400

    feat: add research page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router";
 import { Home } from "./pages/home";
 
 import "./App.css";
+import { Research } from "./pages/research";
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
+      <Route path="research" element={<Research />} />
     </Routes>
   );
 }
---
+++ src/pages/research/index.tsx
@@ -0,0 +1,60 @@
+import { Layout } from "../../layout";
+
+import "./research.css";
+
+export function Research() {
+  return (
+    <Layout>
+      <div className="research-page-content">
+        <p>
+          Between 1950 and 2022, nearly a quarter of US agricultural land exited
+          production, including 65 million acres over the course of a single
+          generation (1992). This is alongside a parallel, but less
+          well-documented, trend of extensive efforts to protect natural
+          landscapes for conservation: approximately 69 million acres of land
+          have entered environmental protection status since 1992.
+        </p>
+        <p>
+          My dissertation project asks 1) what is the extent and nature of
+          agricultural land transitioning to conservation, 2) how do
+          agricultural land transitions change landscapes and the rural
+          communities within them, and 3) what are the perceived implications of
+          change in agricultural land use and tenure for the future of land
+          stewardship in rural America?
+        </p>
+        <p>
+          I explore these questions through a mixed-methods approach that
+          connects cases across various geographic and temporal scales. After
+          drawing on national remote sensing and administrative data to
+          contextualize rural and agricultural land transitions over the last
+          thirty years, I engage in field-based data collection in two
+          locations: the Upper Missouri River Breaks of northeastern Montana and
+          the Kickapoo Valley in the Wisconsin Driftless Area.
+        </p>
+        <p>
+          The transition of US agricultural land to conservation reserves,
+          forming in a moment of heightened concern for both food security and
+          environmental protection, speaks to new land management priorities,
+          shifts in socioeconomic relationships, and altered local place
+          identities. If you are interested in learning more about my research,
+          I invite you to get in touch.
+        </p>
+        <h2>Funding Statement</h2>
+        <p>
+          This research is financially supported by the Rural Sociological
+          Society Dissertation Research Award, Center for Integrated
+          Agricultural Systems, Evelyn T. Crowe Scholarship Fund (Community &
+          Environmental Sociology), Rural Livability Project (Agriculture &
+          Applied Economics), and Department of Sociology at UW-Madison.
+        </p>
+        <h2>Related Publications</h2>
+        <p>
+          Schmidt, Danielle. “Towards an Emplaced Vocabulary of Motive: Senses
+          of Place and Land Sale Decision-Making in the Northern Great Plains.”
+          Rural Sociology 89, no. 4 (2024): 975–95.
+          https://doi.org/10.1111/ruso.12574.
+        </p>
+      </div>
+    </Layout>
+  );
+}
