a0550df8954dbf538d8de56fc7b8cac071559dcc
Nikki Schmidt <nikki@nsylv.dev>
2025-08-09 13:48:27 -0400

    feat: add resources and outreach pages

---
+++ src/pages/outreach/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "../../layout";
+import "./outreach.css";
+
+export function Outreach() {
+  return (
+    <Layout>
+      <div className="outreach-page-content">
+        <h1>Outreach</h1>
+      </div>
+    </Layout>
+  );
+}
--- src/pages/teaching/index.tsx
+++ src/pages/teaching/index.tsx
@@ -1,32 +1,38 @@
+import { Layout } from "../../layout";
+import "./teaching.css";
+
 export function Teaching() {
   return (
-    <div className="teaching-page-content">
-      <p>
-        My approach to teaching emphasizes critical thinking by drawing on
-        students’ lived experiences to develop real-world skills. I believe
-        effective higher education instruction should support students in
-        troubling taken-for-granted truths and prepare them to take on
-        challenges they may confront in any profession. I measure meaningful
-        instruction by my ability to meet a broad range of student needs as they
-        critically engage with new ways of thinking. To do this effectively, I
-        prioritize student autonomy and focus on creating a classroom where
-        students feel safe and encouraged to bring their whole selves.
-      </p>
-      <p>
-        I am interested in teaching courses at the undergraduate or graduate
-        level on the following topics:
-      </p>
-      <ul>
-        <li>Land and landownership</li>
-        <li>Agrifood systems and/or agrarian studies</li>
-        <li>Environmental sociology</li>
-        <li>Rural people, places, and communities</li>
-        <li>Protected areas and conservation</li>
-        <li>Natural resource-based labor</li>
-        <li>Sense of place</li>
-        <li>Methods (qualitative, spatial, mixed, and/or case)</li>
-      </ul>
-      <p>Assessments of my instruction are available upon request.</p>
-    </div>
+    <Layout>
+      <div className="teaching-page-content">
+        <p>
+          My approach to teaching emphasizes critical thinking by drawing on
+          students’ lived experiences to develop real-world skills. I believe
+          effective higher education instruction should support students in
+          troubling taken-for-granted truths and prepare them to take on
+          challenges they may confront in any profession. I measure meaningful
+          instruction by my ability to meet a broad range of student needs as
+          they critically engage with new ways of thinking. To do this
+          effectively, I prioritize student autonomy and focus on creating a
+          classroom where students feel safe and encouraged to bring their whole
+          selves.
+        </p>
+        <p>
+          I am interested in teaching courses at the undergraduate or graduate
+          level on the following topics:
+        </p>
+        <ul>
+          <li>Land and landownership</li>
+          <li>Agrifood systems and/or agrarian studies</li>
+          <li>Environmental sociology</li>
+          <li>Rural people, places, and communities</li>
+          <li>Protected areas and conservation</li>
+          <li>Natural resource-based labor</li>
+          <li>Sense of place</li>
+          <li>Methods (qualitative, spatial, mixed, and/or case)</li>
+        </ul>
+        <p>Assessments of my instruction are available upon request.</p>
+      </div>
+    </Layout>
   );
 }
