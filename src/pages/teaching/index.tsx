6fd65f755050ede3dada6a9ddaa1efca4641822d
Danielle Schmidt <162650766+danielle-schmidt@users.noreply.github.com>
2025-12-17 09:10:58 -0600

    Update index.tsx

--- src/pages/teaching/index.tsx
+++ src/pages/teaching/index.tsx
@@ -12,11 +12,11 @@ export function Teaching() {
         <p>
           My approach to teaching emphasizes critical thinking by drawing on
           students’ lived experiences to develop real-world skills. I believe
-          effective higher education instruction should support students in
+          higher education instruction should support students in
           troubling taken-for-granted truths and prepare them to take on
           challenges they may confront in any profession. I measure meaningful
           instruction by my ability to meet a broad range of student needs as
-          they critically engage with new ways of thinking. To do this
+          they engage with new ways of thinking. To do this
           effectively, I prioritize student autonomy and focus on creating a
           classroom where students feel safe and encouraged to bring their whole
           selves.
