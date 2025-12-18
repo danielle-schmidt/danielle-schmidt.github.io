b4379399fcde130b14c59fe06c609660b8629969
Danielle Schmidt-Larios <162650766+danielle-schmidt@users.noreply.github.com>
2025-12-18 14:27:47 -0600

    Update layout.tsx

--- src/layout.tsx
+++ src/layout.tsx
@@ -17,7 +17,30 @@ export function Layout({ children }: { children?: React.ReactNode }) {
           </nav>
         </div>
       </div>
+      
       <main className="page-content">{children}</main>
+      
+      <div className="site-footer">
+        <div className="footer-column footer-left">
+          <img src="/uw.png" alt="Logo" className="footer-logo" />
+        </div>
+      
+        <div className="footer-column footer-center">
+          <a href="mailto:dschmidt25@wisc.edu">dschmidt25@wisc.edu</a>
+        </div>
+      
+        <div className="footer-column footer-right">
+          <a href="https://scholar.google.com/citations?hl=en&user=AoBIat0AAAAJ&view_op=list_works&authuser=3&gmla=AElLoL22V6QNE-ChvWsjMwDVYIQAne_Lg4kkHPgLgjG1_aDKDZX4KPxivdvgdPXXmqEcDYKTNsolN0zQOmnbbgTX" target="_blank">
+            <img src="/icons/scholar.svg" alt="Scholar" className="footer-icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/danielle-schmidt-2019/" target="_blank">
+            <img src="/icons/linkedin.svg" alt="LinkedIn" className="footer-icon" />
+          </a>
+          <a href="https://github.com/danielle-schmidt" target="_blank">
+            <img src="/icons/github.svg" alt="GitHub" className="footer-icon" />
+          </a>
+        </div>
+      </div>
     </>
   );
 }
