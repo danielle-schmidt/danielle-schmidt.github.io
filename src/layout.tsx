ca0caf4b69dc750dd7bfe5a9d6ee1ee02325cf03
Danielle Schmidt-Larios <162650766+danielle-schmidt@users.noreply.github.com>
2025-12-22 11:52:42 -0600

    Update layout.tsx

--- src/layout.tsx
+++ src/layout.tsx
@@ -25,7 +25,7 @@ export function Layout({ children }: { children?: React.ReactNode }) {
           <img src="/UW logo.png" alt="Logo" className="footer-logo" />
         </div>
         <div className="footer-column footer-left">
-          <img src="/CESoc Transparent Logo.png.png" alt="Logo" className="footer-logo" />
+          <img src="/CESoc Transparent Logo.png" alt="Logo" className="footer-logo" />
           </div>   
         <div className="footer-column footer-center">
           <a href="mailto:dschmidt25@wisc.edu">dschmidt25@wisc.edu</a>
