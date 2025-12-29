ed7e3be825d2adbb08fb3d3c3d7bd265e5386090
Danielle Schmidt-Larios <162650766+danielle-schmidt@users.noreply.github.com>
2025-12-29 10:48:25 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
