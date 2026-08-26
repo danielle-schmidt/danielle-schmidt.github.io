bb903128fc0d4302a97c7fa6f6fd6e14ea8be784
Danielle Schmidt-Larios <162650766+danielle-schmidt@users.noreply.github.com>
2026-08-26 12:38:11 -0500

    Update index.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -22,7 +22,7 @@ export function Home() {
               Community and Environmental Sociology
             </a>
             . Her research sits at the nexus of rural and environmental sociology, with ongoing projects examining land tenure, agricultural labor, and rural 
-            livability. As a community-engaged mixed-methodologist, Schmidt-Larios integrates ethnographic sensibilities and spatial analysis to investigate 
+            livability. As a community-engaged mixed-methodologist, Schmidt-Larios integrates ethnographic sensibilities and spatial thinking to investigate 
             the concepts of place, land, and home. Her research has been published in journals such as <em>Rural Sociology</em> and{" "}
   <em>Agriculture and Human Values</em>, and has received financial support from organizations including the Rural Sociological Society (RSS) 
             and Center for Integrated Agricultural Systems (CIAS).
@@ -31,7 +31,7 @@ export function Home() {
         Schmidt-Larios’s dissertation, <em>Spatiotemporalities of Agricultural Land Loss</em>, explores the social implications of change in land tenure and use. 
             In addition to a national spatial analysis of land exiting agricultural production, the dissertation project puts in conversation 
             two complementary case studies: one in the Breaks of northeastern Montana and the other in the Driftless area of southwestern Wisconsin. 
-            Findings aim to speak to the ways change in land tenure and use reflect evolving environmental and land management priorities, 
+            Findings aim to speak to the ways in which change in land tenure and use reflect evolving environmental and land management priorities, 
             shifts in socioecological relationships, and altered local place identities.
           </p>
           <p>
@@ -42,7 +42,7 @@ export function Home() {
             nonprofit sectors through roles in program management, philanthropic
             consulting, and economic development. Danielle enjoys spending time
             outside and is an avid DIY’er, community volunteer, and horseback
-            rider. She resides in Madison, Wisconsin with her two- and
+            rider. She currently resides in Madison, Wisconsin with her two- and
             four-legged family members: Leo, Chet, Archie, and Maslow.
           </p>
           <div className="cv-link-container">
